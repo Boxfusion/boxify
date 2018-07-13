0bca5acdceebc5c1aa7f5184d214e893aff7627a
Mazi Muhlari <mazi.muhlari@gmail.com>
2018-07-13 15:27:29 +0200

    Updates for links

--- routes/site.js
+++ routes/site.js
@@ -5,4 +5,32 @@ router.get('/', async (req, res) => {
     res.render('index.ejs');
 });
 
+router.get('/qoute/wizard/description', async (req, res) => {
+    res.render('quote-wizard-description.ejs');
+});
+
+router.get('/qoute/wizard/design', async (req, res) => {
+    res.render('quote-wizard-design.ejs');
+});
+
+router.get('/qoute/wizard/audience', async (req, res) => {
+    res.render('quote-wizard-audience.ejs');
+});
+
+router.get('/qoute/wizard/category', async (req, res) => {
+    res.render('quote-wizard-category.ejs');
+});
+
+router.get('/qoute/wizard/features', async (req, res) => {
+    res.render('quote-wizard-features.ejs');
+});
+
+router.get('/qoute/wizard/integrations', async (req, res) => {
+    res.render('quote-wizard-integrations.ejs');
+});
+
+router.get('/qoute/wizard/complete', async (req, res) => {
+    res.render('quote-wizard-complete.ejs');
+});
+
 module.exports = router;
