68c2e29b7e344637fc60de90bd6d11a374f6989d
Mazi Muhlari <mazi.muhlari@gmail.com>
2018-07-16 18:31:09 +0200

    Updates for the category section

--- routes/site.js
+++ routes/site.js
@@ -9,28 +9,62 @@ router.get('/qoute/wizard/description', async (req, res) => {
     res.render('quote-wizard-description.ejs');
 });
 
-router.get('/qoute/wizard/design', async (req, res) => {
-    res.render('quote-wizard-design.ejs');
-});
-
-router.get('/qoute/wizard/audience', async (req, res) => {
-    res.render('quote-wizard-audience.ejs');
-});
-
 router.get('/qoute/wizard/category', async (req, res) => {
     res.render('quote-wizard-category.ejs');
 });
 
-router.get('/qoute/wizard/features', async (req, res) => {
-    res.render('quote-wizard-features.ejs');
+// Event
+
+router.get('/qoute/wizard/category/event/questions', async (req, res) => {
+    res.render('quote-wizard-category-event-questions.ejs');
 });
 
-router.get('/qoute/wizard/integrations', async (req, res) => {
-    res.render('quote-wizard-integrations.ejs');
+router.get('/qoute/wizard/category/event/features', async (req, res) => {
+    res.render('quote-wizard-category-event-features.ejs');
 });
 
-router.get('/qoute/wizard/complete', async (req, res) => {
-    res.render('quote-wizard-complete.ejs');
+// Community
+
+router.get('/qoute/wizard/category/community/questions', async (req, res) => {
+    res.render('quote-wizard-category-community-questions.ejs');
+});
+
+router.get('/qoute/wizard/category/community/features', async (req, res) => {
+    res.render('quote-wizard-category-community-features.ejs');
+});
+
+// Employee
+
+router.get('/qoute/wizard/category/employee/questions', async (req, res) => {
+    res.render('quote-wizard-category-employee-questions.ejs');
+});
+
+router.get('/qoute/wizard/category/employee/features', async (req, res) => {
+    res.render('quote-wizard-category-employee-features.ejs');
+});
+
+// Customer
+
+router.get('/qoute/wizard/category/customer/questions', async (req, res) => {
+    res.render('quote-wizard-category-customer-questions.ejs');
+});
+
+router.get('/qoute/wizard/category/customer/features', async (req, res) => {
+    res.render('quote-wizard-category-customer-features.ejs');
+});
+
+// Other
+
+router.get('/qoute/wizard/category/other/questions', async (req, res) => {
+    res.render('quote-wizard-category-other-questions.ejs');
+});
+
+router.get('/qoute/wizard/category/other/features/', async (req, res) => {
+    res.render('quote-wizard-category-other-features.ejs');
+});
+
+router.get('/qoute/wizard/design', async (req, res) => {
+    res.render('quote-wizard-design.ejs');
 });
 
 module.exports = router;
